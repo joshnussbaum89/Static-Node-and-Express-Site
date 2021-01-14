8f6de853259bd8271fa9695f714bb1400c0d154a
Josh Nussbaum <Joshnussbaum89@gmail.com>
2021-01-13 20:01:10 -0600

    add pictures and change error messages

--- app.js
+++ app.js
@@ -12,22 +12,22 @@ app.use("/static", express.static("public"));
 
 // Routes
 app.get("/", (req, res) => {
-  res.render("index");
+  // renders the index view
+  // locals object whose properties define the local variables for the view.
+  res.render("index", { projects });
 });
 
 // About view
 app.get("/about", (req, res) => {
-  res.render("about");
+  res.render("about", { projects });
 });
 
 // Projects View
 app.get("/project/:id", (req, res, next) => {
-  const { id } = req.params;
-  const found = projects.find((project) => project.id === +id);
-
-  if (found) {
-    res.render("project", { found }); // This pauses execution if project/number > 4 WTF
-  }
+  // renders the view of the project with specific .id
+  // that specific project defines the local variables for the view.
+  // console.log(req.params, projects[req.params.id]);
+  res.render("project", projects[req.params.id]);
 });
 
 // 404 Error
@@ -41,9 +41,19 @@ app.use((req, res, next) => {
 app.use((err, req, res, next) => {
   res.locals.error = err;
   res.status(err.status);
-  res.send(
-    `<h1>Oh no! That's a ${err.status}. Over and out.</h1><h3>${err.message}</h3>`
-  );
+
+  // Errors need to be made nicer. Render different pages for 404 or 500
+  if (err.status === 404) {
+    res
+      .status(404)
+      .send(
+        `<h1>Oh no! That's a ${err.status}. Over and out.</h1><h3>${err.message}</h3>`
+      );
+  } else {
+    err.message = err.message || `Oops!`;
+    res.status(err.status || 500).render("error", { err });
+  }
+
   console.log(err.message, err.status);
 });
 
